68d2093a4418ffdec94f41fa537523e85992b312
Francisco <6652831+franmastromarino@users.noreply.github.com>
2024-03-05 16:51:18 +0100

    Version update
    i18n strings

--- vendor_packages/wp-notice-plugin-promote.php
+++ vendor_packages/wp-notice-plugin-promote.php
@@ -66,9 +66,10 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 				),
 				'notice_description' => sprintf(
 					esc_html__(
-						'Today we have a special gift for you. Use the coupon code %1$s within the next 48 hours to receive a 20% discount on the premium version of the %2$s plugin.',
+						'Today we have a special gift for you. Use the coupon code %1$s within the next 48 hours to receive a %2$s discount on the premium version of the %3$s plugin.',
 						'woo-ajax-add-to-cart'
 					),
+					'20%',
 					'ADMINPANEL20%',
 					QLWCAJAX_PROMOTE_PREMIUM_SELL_NAME
 				),
--- woo-ajax-add-to-cart.php
+++ woo-ajax-add-to-cart.php
@@ -4,7 +4,7 @@
  * Plugin Name:             Ajax add to cart for WooCommerce
  * Plugin URI:              https://quadlayers.com
  * Description:             Ajax add to cart for WooCommerce products
- * Version:                 2.3.2
+ * Version:                 2.3.3
  * Text Domain:             woo-ajax-add-to-cart
  * Author:                  QuadLayers
  * Author URI:              https://quadlayers.com
@@ -14,7 +14,7 @@
  * Tested up to:            6.4
  * Requires PHP:            5.6
  * WC requires at least:    4.0
- * WC tested up to:         8.6
+ * WC tested up to:         8.7
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'QLWCAJAX_PLUGIN_NAME', 'Ajax add to cart for WooCommerce' );
-define( 'QLWCAJAX_PLUGIN_VERSION', '2.3.2' );
+define( 'QLWCAJAX_PLUGIN_VERSION', '2.3.3' );
 define( 'QLWCAJAX_PLUGIN_FILE', __FILE__ );
 define( 'QLWCAJAX_PLUGIN_DIR', __DIR__ . DIRECTORY_SEPARATOR );
 define( 'QLWCAJAX_DOMAIN', 'qlwcajax' );
