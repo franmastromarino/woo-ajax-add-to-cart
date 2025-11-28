2f1ff1b5d6ccf2d7fdbcc4bcd802e3fe54ea5142
Francisco <francisco.mastromarino@urbiport.com>
2025-11-28 20:08:54 -0300

    fix: version update

--- vendor_packages/wp-notice-plugin-promote.php
+++ vendor_packages/wp-notice-plugin-promote.php
@@ -20,18 +20,46 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 		/**
 		 * Notice cross sell 1
 		 */
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_SLUG', 'woocommerce-checkout-manager' );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_NAME', 'WooCommerce Checkout Manager' );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_DESCRIPTION', esc_html__( 'This plugin allows you to add custom fields to the checkout page, related to billing, shipping or additional fields sections.', 'woo-ajax-add-to-cart' ) );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_URL', 'https://quadlayers.com/products/woocommerce-checkout-manager/?utm_source=qlwcajax_plugin&utm_medium=dashboard_notice&utm_campaign=cross_sell&utm_content=checkout_manager_link' );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-checkout-manager.jpg', QLWCAJAX_PLUGIN_FILE ) );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_SLUG', 'wp-whatsapp-chat' );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_NAME', 'Social Chat' );
+		define(
+			'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_TITLE',
+			wp_kses(
+				sprintf(
+					'<h3 style="margin:0">%s</h3>',
+					esc_html__( 'Turn more visitors into customers.', 'woo-ajax-add-to-cart' )
+				),
+				array(
+					'h3' => array(
+						'style' => array()
+					)
+				)
+			)
+		);
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_DESCRIPTION', esc_html__( 'Social Chat allows your users to start a conversation from your website directly to your WhatsApp phone number with one click.', 'woo-ajax-add-to-cart' ) );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_URL', 'https://quadlayers.com/products/whatsapp-chat/?utm_source=qlwcajax_plugin&utm_medium=dashboard_notice&utm_campaign=cross_sell&utm_content=social_chat_link' );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_1_LOGO_SRC', plugins_url( '/assets/backend/img/wp-whatsapp-chat.jpeg', QLWCAJAX_PLUGIN_FILE ) );
 		/**
 		 * Notice cross sell 2
 		 */
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_SLUG', 'perfect-woocommerce-brands' );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_NAME', 'Perfect WooCommerce Brands' );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_DESCRIPTION', esc_html__( 'Perfect WooCommerce Brands the perfect tool to improve customer experience on your site. It allows you to highlight product brands and organize them in lists, dropdowns, thumbnails, and as a widget.', 'woo-ajax-add-to-cart' ) );
-		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_URL', 'https://quadlayers.com/products/perfect-woocommerce-brands/?utm_source=qlwcajax_plugin&utm_medium=dashboard_notice&utm_campaign=cross_sell&utm_content=perfect_brands_link' );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_SLUG', 'woocommerce-checkout-manager' );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_NAME', 'WooCommerce Checkout Manager' );
+		define(
+			'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_TITLE',
+			wp_kses(
+				sprintf(
+					'<h3 style="margin:0">%s</h3>',
+					esc_html__( 'Customize your checkout in minutes.', 'woo-ajax-add-to-cart' )
+				),
+				array(
+					'h3' => array(
+						'style' => array()
+					)
+				)
+			)
+		);
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_DESCRIPTION', esc_html__( 'WooCommerce Checkout Manager allows you to add custom fields to the checkout page, related to billing, Shipping or Additional fields sections.', 'woo-ajax-add-to-cart' ) );
+		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_URL', 'https://quadlayers.com/products/woocommerce-checkout-manager/?utm_source=qlwcajax_plugin&utm_medium=dashboard_notice&utm_campaign=cross_sell&utm_content=checkout_manager_link' );
 		define( 'QLWCAJAX_PROMOTE_CROSS_INSTALL_2_LOGO_SRC', plugins_url( '/assets/backend/img/woocommerce-checkout-manager.jpg', QLWCAJAX_PLUGIN_FILE ) );
 
 		new \QuadLayers\WP_Notice_Plugin_Promote\Load(
@@ -41,12 +69,18 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					'type'               => 'ranking',
 					'notice_delay'       => 0,
 					'notice_logo'        => QLWCAJAX_PROMOTE_LOGO_SRC,
-					'notice_description' => sprintf(
-									esc_html__( 'Hello! %2$s We\'ve spent countless hours developing this free plugin for you and would really appreciate it if you could drop us a quick rating. Your feedback is extremely valuable to us. %3$s It helps us to get better. Thanks for using %1$s.', 'woo-ajax-add-to-cart' ),
-									'<b>'.QLWCAJAX_PLUGIN_NAME.'</b>',
-									'<span style="font-size: 16px;">🙂</span>',
-									'<br>'
+					'notice_title'       => wp_kses(
+						sprintf(
+							'<h3 style="margin:0">%s</h3>',
+							esc_html__( 'Enjoying WooCommerce Ajax Add to Cart?', 'woo-ajax-add-to-cart' )
+						),
+						array(
+							'h3' => array(
+								'style' => array()
+							)
+						)
 					),
+					'notice_description' => esc_html__( 'A quick 5-star review helps us keep improving the plugin and supporting users like you. It only takes 2 seconds — thank you!', 'woo-ajax-add-to-cart' ),
 					'notice_link'        => QLWCAJAX_PROMOTE_REVIEW_URL,
 					'notice_more_link'   => 'https://quadlayers.com/account/support/?utm_source=qlwcajax_plugin&utm_medium=dashboard_notice&utm_campaign=support&utm_content=report_bug_button',
 					'notice_more_label'  => esc_html__(
@@ -63,9 +97,16 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					),
 					'notice_delay'       => WEEK_IN_SECONDS,
 					'notice_logo'        => QLWCAJAX_PROMOTE_LOGO_SRC,
-					'notice_title'       => esc_html__(
-						'Hello! We have a special gift!',
-						'woo-ajax-add-to-cart'
+					'notice_title'       => wp_kses(
+						sprintf(
+							'<h3 style="margin:0">%s</h3>',
+							esc_html__( 'Save 20% today!', 'woo-ajax-add-to-cart' )
+						),
+						array(
+							'h3' => array(
+								'style' => array()
+							)
+						)
 					),
 					'notice_description' => sprintf(
 						esc_html__(
@@ -82,13 +123,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					'plugin_slug'        => QLWCAJAX_PROMOTE_CROSS_INSTALL_1_SLUG,
 					'notice_delay'       => MONTH_IN_SECONDS * 3,
 					'notice_logo'        => QLWCAJAX_PROMOTE_CROSS_INSTALL_1_LOGO_SRC,
-					'notice_title'       => sprintf(
-						esc_html__(
-							'Hello! We want to invite you to try our %s plugin!',
-							'woo-ajax-add-to-cart'
-						),
-						QLWCAJAX_PROMOTE_CROSS_INSTALL_1_NAME
-					),
+					'notice_title'       => QLWCAJAX_PROMOTE_CROSS_INSTALL_1_TITLE,
 					'notice_description' => QLWCAJAX_PROMOTE_CROSS_INSTALL_1_DESCRIPTION,
 					'notice_more_link'   => QLWCAJAX_PROMOTE_CROSS_INSTALL_1_URL
 				),
@@ -96,13 +131,7 @@ if ( class_exists( 'QuadLayers\\WP_Notice_Plugin_Promote\\Load' ) ) {
 					'plugin_slug'        => QLWCAJAX_PROMOTE_CROSS_INSTALL_2_SLUG,
 					'notice_delay'       => MONTH_IN_SECONDS * 6,
 					'notice_logo'        => QLWCAJAX_PROMOTE_CROSS_INSTALL_2_LOGO_SRC,
-					'notice_title'       => sprintf(
-						esc_html__(
-							'Hello! We want to invite you to try our %s plugin!',
-							'woo-ajax-add-to-cart'
-						),
-						QLWCAJAX_PROMOTE_CROSS_INSTALL_2_NAME
-					),
+					'notice_title'       => QLWCAJAX_PROMOTE_CROSS_INSTALL_2_TITLE,
 					'notice_description' => QLWCAJAX_PROMOTE_CROSS_INSTALL_2_DESCRIPTION,
 					'notice_more_link'   => QLWCAJAX_PROMOTE_CROSS_INSTALL_2_URL
 				),
